195dcf544e348f5e3522bd8a3d34425c5d203b11
Shintenza <kam.kuziora@gmail.com>
2021-10-07 20:14:26 +0200

    updated nvim tree config

--- lua/configs/nvim-tree.lua
+++ lua/configs/nvim-tree.lua
@@ -1,35 +1,36 @@
 vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true})
+require'nvim-tree'.setup {
+  disable_netrw       = true,
+  hijack_netrw        = true,
+  open_on_setup       = false,
+  ignore_ft_on_setup  = {},
+  auto_close          = true,
+  open_on_tab         = false,
+  update_to_buf_dir   = {
+    enable = true,
+    auto_open = true,
+  },
+  hijack_cursor       = false,
+  update_cwd          = false,
+  lsp_diagnostics     = true,
+  update_focused_file = {
+    enable      = false,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
 
-vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_indent_markers = 0 
-vim.g.nvim_tree_tab_open = 1
-vim.g.nvim_tree_update_cwd = 1
-vim.g.nvim_tree_auto_open = 1
-vim.g.nvim_tree_follow = 1
-vim.g.nvim_tree_side = "left"
-vim.g.nvim_tree_disable_netrw = 1
-
-vim.g.nvim_tree_icons = {
-   default = "",
-   symlink = "",
-   git = {
-      deleted = "",
-      ignored = "◌",
-      renamed = "➜",
-      staged = "✓",
-      unmerged = "",
-      unstaged = "✗",
-      untracked = "★",
-   },
-   folder = {
-      -- disable indent_markers option to get arrows working or if you want both arrows and indent then just add the arrow icons in front            ofthe default and opened folders below!
-      -- arrow_open = "",
-      -- arrow_closed = "",
-      default = "",
-      empty = "", -- 
-      empty_open = "",
-      open = "",
-      symlink = "",
-      symlink_open = "",
-   },
+  view = {
+    width = 30,
+    height = 30,
+    side = 'left',
+    auto_resize = false,
+    mappings = {
+      custom_only = false,
+      list = {}
+    }
+  }
 }
