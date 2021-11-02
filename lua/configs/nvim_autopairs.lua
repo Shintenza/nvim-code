f4e5ce7ef24e99cef921f1cef7e5c1f0b43d3361
Shintenza <kam.kuziora@gmail.com>
2021-11-02 23:03:59 +0100

    fixed autpairs config

--- lua/configs/nvim_autopairs.lua
+++ lua/configs/nvim_autopairs.lua
@@ -1,4 +1,6 @@
-local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-local cmp = require('cmp')
-cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
-
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+require("cmp").event:on(
+	"confirm_done",
+	cmp_autopairs.on_confirm_done()
+)
+require('nvim-autopairs').setup{}
