2c9ed502ebe618b00a802657e8bb599b30572dc2
Shintenza <kam.kuziora@gmail.com>
2022-03-21 23:11:01 +0100

    removed indent marks from dialogs/help...

--- lua/configs/indent-blankline.lua
+++ lua/configs/indent-blankline.lua
@@ -1,7 +1,7 @@
 local status_ok, indent_blankline = pcall(require, "indent_blankline")
 
 indent_blankline.setup {
-  filetype_exclude = {'NvimTree'},
+  filetype_exclude = {'NvimTree', 'lspinfo', 'help', 'packer', 'checkhealth', ''},
   use_treesitter = true,
   buftype_exclude = {'terminal'},
   show_first_indent_level = false
