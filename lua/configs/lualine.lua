fe1261b45c7e0301065ed63fdd567ef8f7f30690
Kamil Kuziora <kam.kuziora@gmail.com>
2022-04-30 21:16:20 +0200

    custom lualine

--- lua/configs/lualine.lua
+++ lua/configs/lualine.lua
@@ -7,18 +7,100 @@ lualine.setup {
   options = {
     icons_enabled = true,
     theme = 'auto',
-    component_separators = { left = '', right = ''},
-    section_separators = { left = '', right = ''},
     always_divide_middle = true,
     globalstatus = true,
   },
   sections = {
-    lualine_a = {'mode'},
-    lualine_b = {'branch', 'diff', 'diagnostics'},
-    lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
-    lualine_y = {'progress'},
-    lualine_z = {'location'}
+    lualine_a = {
+        {
+            function()
+                return 'ﮊ'
+            end,
+            color = { fg = '#000000', bg = "#E06C75" }, -- Sets highlighting of component
+            padding = { left = 1, right = 1 }, -- We don't need space before this
+            separator = {right = ' '},
+        }
+    },
+    lualine_b = {
+        {
+            'filetype',
+            color = { fg = '#ffffff', bg = '#2C3039' },
+            icon_only = true,
+            separator = {right = ''},
+        },
+        {
+            'filename',
+            color = { fg = '#ffffff', bg = '#2C3039' },
+            separator = {right = ' '},
+        },
+        {
+            'branch',
+            color = { fg = '#ffffff', bg = '#262A32'},
+            separator = {right = ' '},
+
+        },
+        {
+            'diff',
+            color = { fg = '#ffffff', bg = '#22262E'},
+            symbols = {added = '落', modified = 'ﱴ ', removed = ' '},
+            separator = {right = ' '},
+            colored = false,
+        },
+    },
+    lualine_c = {},
+    lualine_x = {
+        {
+            'diagnostics',
+            symbols = {error = ' ', warn = ' ', info = ' ', hint = 'H'},
+            color = { fg = '#ffffff', bg = '#22262E'},
+            separator = { left = ' ' },
+        },
+        {
+             function()
+                local msg = 'No Active Lsp'
+                local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+                local clients = vim.lsp.get_active_clients()
+                if next(clients) == nil then
+                  return msg
+                end
+                for _, client in ipairs(clients) do
+                  local filetypes = client.config.filetypes
+                  if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+                    return client.name
+                  end
+                end
+                return msg
+              end,
+              icon = ' LSP:',
+              color = { fg = '#ffffff', bg = '#22262E' },
+              separator = { left = ' ' },
+        },
+    },
+    lualine_y = {
+        {
+            function()
+                return ''
+            end,
+            color = { fg = '#ffffff', bg = '#2C3039' }, -- Sets highlighting of component
+            padding = { left = 1, right = 1 }, -- We don't need space before this
+            separator = { left = ' ' },
+        },
+        {
+            'mode',
+            color = { fg = '#ffffff', bg = '#2D3139' },
+        },
+    },
+    lualine_z = {
+        {
+            function()
+                return ' '
+            end,
+            color = { fg = '#000000', bg = '#E06C75' },
+            padding = { left = 1, right = 1 }, -- We don't need space before this
+            separator = { left = ' ' },
+            padding = { right = 1 },
+        },
+    }
   },
   inactive_sections = {
     lualine_a = {},
