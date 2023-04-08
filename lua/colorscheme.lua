dd6e49e0f063d59947d7ec5dcb677cf28e875177
Kamil Kuziora <kam.kuziora@gmail.com>
2023-04-08 17:43:19 +0200

    fixed nvimtree theme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -9,3 +9,8 @@ if vim.fn.hlexists('SagaBorder') and vim.fn.hlexists('SagaNormal') then
     vim.cmd('highlight! link SagaBorder SagaNormal')
 end
 
+
+if vim.fn.hlexists('NvimTreeNormal') and vim.fn.hlexists('NvimTreeWinSeparator') then
+    local bg = vim.api.nvim_get_hl_by_name("NvimTreeNormal", true).guibg
+    vim.cmd("highlight NvimTreeWinSeparator guifg=bg")
+end
