3b6b01ae0521390b0a625ad886a2b1401c72fbc2
Kamil Kuziora <kam.kuziora@gmail.com>
2024-02-02 12:40:09 +0100

    autopairs update

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,6 +1,6 @@
 local M = {
   "windwp/nvim-autopairs",
-  commit = "ae5b41ce880a6d850055e262d6dfebd362bb276e",
+  commit = "096d0baecc34f6c5d8a6dd25851e9d5ad338209b",
   event = "InsertEnter",
   dependencies = {
     {
@@ -16,12 +16,6 @@ local M = {
 function M.config()
   require("nvim-autopairs").setup({
     check_ts = true, -- treesitter integration
-    disable_filetype = { "TelescopePrompt" },
-    ts_config = {
-      lua = { "string", "source" },
-      javascript = { "string", "template_string" },
-      java = false,
-    },
     fast_wrap = {
       map = "<M-e>",
       chars = { "{", "[", "(", '"', "'" },
