fc686804095098062eb6f93ec21270862769473e
Kamil Kuziora <kam.kuziora@gmail.com>
2024-02-02 13:03:02 +0100

    telescope config update

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,13 +11,22 @@ function M.config()
 
   telescope.setup({
     defaults = {
-      prompt_prefix = " ",
-      selection_caret = " ",
-      path_display = { "smart" },
-      file_ignore_patterns = { ".git/", "node_modules" },
+      prompt_prefix = "   ",
+      selection_caret = "  ",
+      entry_prefix = "  ",
+      initial_mode = "insert",
+      selection_strategy = "reset",
+      sorting_strategy = "ascending",
+      layout_strategy = "horizontal",
       layout_config = {
-        horizontal = { prompt_position = "top", preview_width = 0.55 },
-        vertical = { mirror = false },
+        horizontal = {
+          prompt_position = "top",
+          preview_width = 0.55,
+          results_width = 0.8,
+        },
+        vertical = {
+          mirror = false,
+        },
         width = 0.87,
         height = 0.80,
         preview_cutoff = 120,
