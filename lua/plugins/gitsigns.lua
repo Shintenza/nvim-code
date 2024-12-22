f2f42db1faa84c4adeae24969fa2b1814030ee56
Kamil Kuziora <kam.kuziora@gmail.com>
2024-12-23 00:12:06 +0100

    fix: broken lsp on attach, gitsigns warning

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,21 +8,11 @@ function M.config()
 
   gitsigns.setup({
     signs = {
-      add = { hl = "GitSignsAdd", text = "▎", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-      change = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-      delete = { hl = "GitSignsDelete", text = "󰐊", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-      topdelete = {
-        hl = "GitSignsDelete",
-        text = "󰐊",
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
-      },
-      changedelete = {
-        hl = "GitSignsChange",
-        text = "▎",
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
-      },
+      add = { text = "▎" },
+      change = { text = "▎" },
+      delete = { text = "󰐊" },
+      topdelete = { text = "󰐊" },
+      changedelete = { text = "▎" },
     },
     signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
     watch_gitdir = {
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,9 +1,56 @@
 local M = {
   "neovim/nvim-lspconfig",
   lazy = false,
-  event = { BufReadPre },
+  event = { "BufReadPre", "BufNewFile" },
   dependencies = { "hrsh7th/cmp-nvim-lsp" },
 }
+
+local function lsp_keymaps(bufnr)
+  local opts = { noremap = false, silent = true }
+  local keymap = vim.api.nvim_buf_set_keymap
+
+  keymap(bufnr, 'n', 'gD',               '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  keymap(bufnr, 'n', 'gd',               '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+  keymap(bufnr, 'n', 'gi',               '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+  keymap(bufnr, 'n', 'gr',               '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+  keymap(bufnr, 'n', '<C-k>',            '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+  keymap(bufnr, 'n', '<leader>wa',       '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+  keymap(bufnr, 'n', '<leader>wr',       '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+  keymap(bufnr, 'n', '<leader>wl',       '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+  keymap(bufnr, 'n', '<leader>fr',       '<cmd>lua vim.lsp.buf.format({async = true})<CR>', opts)
+  keymap(bufnr, 'n', '<leader>D',        '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+  keymap(bufnr, 'n', '<leader>e',        '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+  keymap(bufnr, 'n', '<leader>q',        '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+  -- Lspsaga
+  keymap(bufnr, "n", "gh",               "<cmd>Lspsaga lsp_finder<CR>", opts)
+  keymap(bufnr, "n", "<leader>ca",       "<cmd>Lspsaga code_action<CR>", opts)
+  keymap(bufnr, "v", "<leader>ca",       "<cmd><C-U>Lspsaga range_code_action<CR>", opts)
+  keymap(bufnr, "n", "K",                "<cmd>Lspsaga hover_doc<CR>", { silent = true })
+  keymap(bufnr, "n", "<leader>rn",       "<cmd>Lspsaga rename<CR>", opts)
+  keymap(bufnr, "n", "<leader>cd",       "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+  keymap(bufnr, "n", "<leader>o",        "<cmd>Lspsaga outline<CR>", opts)
+  keymap(bufnr, "n", "<leader>t",        "<cmd>Lspsaga term_toggle<CR>", opts)
+  keymap(bufnr, "n", "[d",               "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+  keymap(bufnr, "n", "]d",               "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+end
+
+M.on_attach = function(client, bufnr)
+  lsp_keymaps(bufnr)
+  if client.name == "tsserver" then
+    client.server_capabilities.document_formatting = false
+  end
+  if client.name == "jdt.ls" then
+    client.server_capabilities.document_formatting = false
+    client.server_capabilities.textDocument.completion.completionItem.snippetSupport = false
+  end
+
+  local status_ok, illuminate = pcall(require, "illuminate")
+  if not status_ok then
+    return
+  end
+  illuminate.on_attach(client)
+end
 
 function M.config()
   local cmp_nvim_lsp = require("cmp_nvim_lsp")
@@ -37,53 +84,6 @@ function M.config()
   }
 
   vim.diagnostic.config(config)
-
-  local function lsp_keymaps(bufnr)
-    local opts = { noremap = true, silent = true }
-    local keymap = vim.api.nvim_buf_set_keymap
-
-    keymap(bufnr, 'n', 'gD',               '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    keymap(bufnr, 'n', 'gd',               '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    keymap(bufnr, 'n', 'gi',               '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    keymap(bufnr, 'n', 'gr',               '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    keymap(bufnr, 'n', '<C-k>',            '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    keymap(bufnr, 'n', '<leader>wa',       '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    keymap(bufnr, 'n', '<leader>wr',       '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    keymap(bufnr, 'n', '<leader>wl',       '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    keymap(bufnr, 'n', '<leader>fr',       '<cmd>lua vim.lsp.buf.format({async = true})<CR>', opts)
-    keymap(bufnr, 'n', '<leader>D',        '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    keymap(bufnr, 'n', '<leader>e',        '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-    keymap(bufnr, 'n', '<leader>q',        '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
-
-    -- Lspsaga
-    keymap(bufnr, "n", "gh",               "<cmd>Lspsaga lsp_finder<CR>", opts)
-    keymap(bufnr, "n", "<leader>ca",       "<cmd>Lspsaga code_action<CR>", opts)
-    keymap(bufnr, "v", "<leader>ca",       "<cmd><C-U>Lspsaga range_code_action<CR>", opts)
-    keymap(bufnr, "n", "K",                "<cmd>Lspsaga hover_doc<CR>", { silent = true })
-    keymap(bufnr, "n", "<leader>rn",       "<cmd>Lspsaga rename<CR>", opts)
-    keymap(bufnr, "n", "<leader>cd",       "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-    keymap(bufnr, "n", "<leader>o",        "<cmd>Lspsaga outline<CR>", opts)
-    keymap(bufnr, "n", "<leader>t",        "<cmd>Lspsaga term_toggle<CR>", opts)
-    keymap(bufnr, "n", "[d",               "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
-    keymap(bufnr, "n", "]d",               "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-  end
-
-  M.on_attach = function(client, bufnr)
-    lsp_keymaps(bufnr)
-    if client.name == "tsserver" then
-      client.server_capabilities.document_formatting = false
-    end
-    if client.name == "jdt.ls" then
-      client.server_capabilities.document_formatting = false
-      client.server_capabilities.textDocument.completion.completionItem.snippetSupport = false
-    end
-
-    local status_ok, illuminate = pcall(require, "illuminate")
-    if not status_ok then
-      return
-    end
-    illuminate.on_attach(client)
-  end
 end
 
 return M
